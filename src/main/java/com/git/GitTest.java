8d27f14cd4dd0d74c31fdcd00e37283fcde15a5d
lxs <861795821@qq.com>
2021-06-16 21:13:27 +0800

    push commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("git提交4");
         System.out.println("git测试master");
         System.out.println("git测试hot-fix");
+        System.out.println("git测试push test");
     }
 }
