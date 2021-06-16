11520dfaa9a297e6d1cf21af99af9f951e7ad2f9
lxs <861795821@qq.com>
2021-06-16 20:50:01 +0800

    hot-fix commit1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git测试3");
         System.out.println("git提交3");
+        System.out.println("git测试4");
+        System.out.println("git提交4");
     }
 }
