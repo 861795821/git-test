bb2c609ff5b2f78f7d0937a7816f39e9452ff4d7
lxs <861795821@qq.com>
2021-06-16 20:38:23 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,6 +3,7 @@ package com.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("git测试");
+        System.out.println("git测试2");
+        System.out.println("git提交2");
     }
 }
